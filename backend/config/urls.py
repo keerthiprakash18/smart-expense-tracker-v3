a635ea7a23ec16481eea4e166da75f4df94e7c01
KEERTHI PRAKASH T <keerthiprakash.kp33@gmail.com>
2026-09-21 17:48:27 +0530

    Use flexible email or username login endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from expenses.auth_views import LoginView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/", LoginView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("expenses.urls")),
 ]
